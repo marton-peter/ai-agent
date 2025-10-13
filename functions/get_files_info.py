882c0f2cde409bc484253a72651ce2eac8ea7b9d
marton-peter <marton.peter85@gmail.com>
2025-10-13 15:55:50 +0200

    get_files_info added

---
+++ functions/get_files_info.py
@@ -0,0 +1,22 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    fullpath = os.path.abspath(os.path.join(working_directory, directory))
+    if not fullpath.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(fullpath):
+        return f'Error: "{directory}" is not a directory'
+    
+    try:
+        contents = os.listdir(fullpath)
+        results = []
+
+        for item in contents:
+            results.append(f" - {item}: file_size={os.path.getsize(os.path.join(fullpath, item))}, is_dir={os.path.isdir(os.path.join(fullpath, item))}")
+    
+    except Exception as e:
+        return f"Error: {e}"
+
+
+    
+    return "\n".join(results)
--- main.py
+++ main.py
@@ -2,23 +2,36 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 if len(sys.argv) < 2:
     sys.stderr.write("no prompt provided\n")
     sys.exit(1)
 
-if sys.argv[1] == "":
+user_prompt = sys.argv[1]
+if user_prompt == "":
     sys.stderr.write("empty prompt provided\n")
     sys.exit(1)
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+verbose = False
+if "--verbose" in sys.argv:
+    verbose = True
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=sys.argv[1]
+    model='gemini-2.0-flash-001', contents=messages
 )
+if verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 print(response.text)
-print(
-    f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
-    f"Response tokens: {response.usage_metadata.candidates_token_count}"
-)
+
