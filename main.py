aad262716db867da383e4309092dfcd3148ef7ac
marton-peter <marton.peter85@gmail.com>
2025-11-07 15:39:55 +0100

    feedback loop added

--- main.py
+++ main.py
@@ -38,39 +38,53 @@ available_functions = types.Tool(
     ]
 )
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model=model_name,
-    contents=messages,
-    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
-)
+try:
+    counter = 1
+    while counter < 20:
+        load_dotenv()
+        api_key = os.environ.get("GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
+        response = client.models.generate_content(
+            model=model_name,
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+        )
+        counter += 1
+        
+        for candidate in response.candidates: # Add response candidates to message history
+            messages.append(candidate.content)
 
-if verbose: # Print metadata if the verbose argument is present
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if verbose: # Print metadata if the verbose argument is present
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-if response.function_calls and len(response.function_calls) > 0: # Check for called functions and print them
-    for call in response.function_calls:
-        function_call_result = call_function(call, verbose=verbose)
+        if response.function_calls and len(response.function_calls) > 0: # Check for called functions, print them, and add them to the message history
+            for call in response.function_calls:
+                function_call_result = call_function(call, verbose=verbose)
+                messages.append(function_call_result)
+                messages.append(types.Content(role="user", parts=function_call_result.parts))
 
-        # parts exists and has at least one Part
-        if not getattr(function_call_result, "parts", None) or len(function_call_result.parts) == 0:
-            raise RuntimeError("call_function returned Content with no parts")
+                # parts exists and has at least one Part
+                if not getattr(function_call_result, "parts", None) or len(function_call_result.parts) == 0:
+                    raise RuntimeError("call_function returned Content with no parts")
 
-        part0 = function_call_result.parts[0]
-        fr = getattr(part0, "function_response", None)
-        if fr is None:
-            raise RuntimeError("missing function_response on Content.part[0]")
+                part0 = function_call_result.parts[0]
+                fr = getattr(part0, "function_response", None)
+                if fr is None:
+                    raise RuntimeError("missing function_response on Content.part[0]")
 
-        resp = getattr(fr, "response", None)
-        if not resp:
-            raise RuntimeError("missing function_response.response")
+                resp = getattr(fr, "response", None)
+                if not resp:
+                    raise RuntimeError("missing function_response.response")
 
-        if verbose:
-            print(f"-> {resp}")
-else:
-    print(response.text)
-
+                if verbose:
+                    print(f"-> {resp}")
+            continue
+        elif response.text and len(response.text) > 0: # If no functions were called and a final text response exists, print it and break out
+                    print(response.text)
+                    break
+        else:
+             continue
+except Exception as e:
+    print(f"Error: {e}")
